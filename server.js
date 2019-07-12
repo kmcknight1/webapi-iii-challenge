0671089981a7fe6f7f761b9c2c849cb9a452bff5
kmcknight1 <katiemcknight1991@gmail.com>
2019-07-11 21:15:44 -0500

    updates server.get / to display instructions

--- server.js
+++ server.js
@@ -23,7 +23,11 @@ server.use("/api/users", userRouter);
 
 //global / endpoint
 server.get("/", (req, res) => {
-  res.send(`<h2>Hello! Check out this ${process.env.EV}!</h2>`);
+  res.send(
+    `<h1>Hello! Check out this ${
+      process.env.EV
+    }!</h1><h2>Add the endpoints below to the url to see data!</h2></br><h3>Endpoints:</h3><ol><li>/api/users</li><li>/api/users/:id</li><li>/api/users/:id/posts</li></ol><h3>Data to expect:</h3><ol><li>Lists all users, names and ids</li><li>Pass in a valid user id to see that user's name & id</li><li>Pass in a valid user id to list all posts made by that user</li></ol>`
+  );
 });
 
 module.exports = server;
